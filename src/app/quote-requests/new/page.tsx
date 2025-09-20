3a14ef999b7417824843e1b9ffd00eefc0274edb
Mike <e-business@live.nl>
2025-09-20 22:11:34 +0200

    DEBUG: Add customer number debugging to investigate template application

--- src/app/quote-requests/new/page.tsx
+++ src/app/quote-requests/new/page.tsx
@@ -165,12 +165,24 @@ const NewQuoteRequestPage = () => {
 
   // Update customer number when involved country changes
   useEffect(() => {
+    console.log('Customer number useEffect triggered:', {
+      customerId,
+      involvedCountry,
+      customerDetails: customerDetails?.customerNumbers,
+      currentCustomerNumber: customerNumber
+    });
+    
     if (customerId && involvedCountry && customerDetails?.customerNumbers) {
       // Only set customer number if it's not already set from template
       if (!customerNumber) {
-        setCustomerNumber(customerDetails.customerNumbers[involvedCountry] || "");
+        const newCustomerNumber = customerDetails.customerNumbers[involvedCountry] || "";
+        console.log('Setting customer number from customer data:', newCustomerNumber);
+        setCustomerNumber(newCustomerNumber);
+      } else {
+        console.log('Customer number already set, not overwriting');
       }
     } else {
+      console.log('Clearing customer number - missing data');
       setCustomerNumber("");
     }
   }, [customerId, involvedCountry, customerDetails, customerNumber]);
@@ -241,7 +253,10 @@ const NewQuoteRequestPage = () => {
       setJobsiteContactId(template.templateData.defaultJobsiteContactId);
     }
     if (template.templateData.defaultCustomerNumber) {
+      console.log('Setting customerNumber from template:', template.templateData.defaultCustomerNumber);
       setCustomerNumber(template.templateData.defaultCustomerNumber);
+    } else {
+      console.log('No defaultCustomerNumber in template data');
     }
     if (template.templateData.defaultNotes) {
       setNotes(template.templateData.defaultNotes);
